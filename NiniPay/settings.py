e03ef665aab41094ac4cc9e98bee158e634d61e0
a-jafarzadeh <a.jafarzadeh1998@gmail.com>
2020-06-23 21:18:23 +0000

    DB switch to Postgre Sql

--- NiniPay/settings.py
+++ NiniPay/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'NiniPay.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ninipaydb',
+        'USER': 'niniadmin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
 
